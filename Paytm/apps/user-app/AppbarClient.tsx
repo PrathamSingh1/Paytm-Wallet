bc09b1fce30a719ebeb20e381ab00d5ac56b919a
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-25 12:21:47 +0000

    Adding dashboard

--- Paytm/apps/user-app/AppbarClient.tsx
+++ Paytm/apps/user-app/AppbarClient.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import { Appbar } from "@repo/ui/appbar";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 
 
