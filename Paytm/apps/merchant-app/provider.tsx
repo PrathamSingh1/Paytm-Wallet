129ba4f2b49d3844fde54c5aa01c110a14aef024
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-25 11:45:22 +0000

    Added Provider in merchant-app

--- Paytm/apps/merchant-app/provider.tsx
+++ Paytm/apps/merchant-app/provider.tsx
@@ -1,6 +1,10 @@
 "use client"
-import { RecoilRoot } from "recoil";
+
+import {RecoilRoot} from "recoil";
 import { SessionProvider } from "next-auth/react";
+import { Children } from "react";
+
+
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
         <SessionProvider>
