86e755561369515432e46eaa2d380635a59f865b
PrathamSingh1 <prathamjii36@gmail.com>
2025-07-31 19:47:17 +0000

    Adding upsert

--- Paytm/apps/merchant-app/app/api/user/route.ts
+++ Paytm/apps/merchant-app/app/api/user/route.ts
@@ -4,15 +4,16 @@ import { PrismaClient } from "@repo/db/client";
 const client = new PrismaClient();
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-        email: "asd",
-        name: "adsads",
-        number: "1234567890",        
-        password: "yourHashedPassword" 
-        }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
+  await client.user.upsert({
+    where: { email: "asd" },  // If you want to upsert on email, adjust as needed
+    update: {},               // No fields to update
+    create: {
+      email: "asd",
+      name: "adsads",
+      number: "1234567890",
+      password: "yourHashedPassword"
+    }
+  });
+
+  return NextResponse.json({ message: "user inserted or already existed" });
 }
